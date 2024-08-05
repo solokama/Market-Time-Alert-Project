69b2b0cd489e06125310909729167e5a20d46942
Its_Irakli <50860718+solokama@users.noreply.github.com>
2024-08-05 14:58:26 +0300

    First Commit

---
+++ Time Project/main.js
@@ -0,0 +1,19 @@
+//if the hour is betweeen 6am and 12pm : Display Good morning
+//if the hour is between 12pm and 6pm :Display Good afternoon
+//Otherwise Good evening!
+
+
+
+let hour =19;
+if(hour >=6 && hour < 12)
+    console.log("Welcome to Halvana Market, Good morning!!! Seller, Today is your Day🤳🤳🤳🤳🤳🤳🤳🤳🤳💕💕💕🎁🎁🎁🎁");
+
+else if(hour >=12 && hour < 18)
+    console.log("Good afternoon,an early bird catches the fattest worm, but in your case, God will provide!'👌👌👌👌👌👌👌");
+else if(hour >18 )
+    console.log("Yieeeeeeeeeeeeeeeeeeeeeshh Today's Market ,✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨ Time is Over!!!");
+
+else 
+    console.log("Good evening");
+
+ 
